2025a0ff6362d4f656f31b0dc944879943f97680
Brian <45672580+brianrcaskey@users.noreply.github.com>
2020-01-18 11:01:33 -0600

    Variable Initialization

---
+++ F5MAIN.m
@@ -0,0 +1,17 @@
+%Aer 351 Code 
+%Written by Brian Caskey
+%1/18/2020
+
+clear,clc,close all
+
+weight = 12000; %lb
+Ixx = 2620; %slug-ft^2
+Iyy = 30300; %slug-ft^2
+Izz = 32300; %slug-ft^2
+Ixy = 0; %slug-ft^2
+Ixz = -190; %slug-ft^2
+Iyz = 0; %slug-ft^2
+S = 173.82; %ft^2 - Wing ref area
+b = 25.25; %ft - wingspan
+c_bar = 7.73; %ft - mean aerodynamic chord
+
