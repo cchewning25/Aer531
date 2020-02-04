912a5dc92ddcc9a0b9dd6e84c6f26a9eb15ee50a
Brian <45672580+brianrcaskey@users.noreply.github.com>
2020-02-04 16:57:26 -0600

    Add files via upload

--- LiftDragMoment.m
+++ LiftDragMoment.m
@@ -2,13 +2,21 @@ clear,clc
 
 
 
-function LDM = LiftDragMoment(q_bar, alpha)
+%function LDM = LiftDragMoment(q_bar, alpha)
 
+alpha = 2*pi/180; %temporary input - AoA in rads
+V = 176;           %temporary input - velocity in ft/s
+delta_e = 2*pi/180;
+
+q = .5*(V^2)*0.0765;
+
+%Define import options
 opts = detectImportOptions('LiftDragMoment_Coeff.xlsx'); %detects import options
 opts.SelectedVariableNames = [3];   %imports only 3rd column of data
 
 T = readmatrix('LiftDragMoment_Coeff.xlsx', opts);
 
+%Define coefficients for buildup
 CL_0_w      = T(1);
 CL_alpha_w  = T(2);
 CD_min_w    = T(3);
@@ -19,19 +27,35 @@ CL_alpha_h  = T(7);
 CD_min_h    = T(8);
 CM_alpha_f  = T(9);
 CD_f        = T(10);
+S_w         = T(11); %reference wing area
+c_w         = T(12); %reference chord
+b_w         = T(13); %reference span
+x_cg        = T(14);
+z_cg        = T(15);
+i_w         = T(16);
+i_t         = T(17);
+e_w         = T(18); %Oswald efficiency factor
+cg_w        = T(19);
+cg_t        = T(20);
+T_e         = T(21);
+e_t         = T(22);
+b_t         = T(23);
+c_t         = T(24);
 
-
-S           = T(11); %reference wing area
-c           = T(12); %reference chord
-b           = T(13); %reference span
-AR_w        = b/c;
+%Factors derived from aircraft dimensions
+AR_w    = b_w/c_w;
+AR_t    = b_t/c_t;
+K_w     = 1/(pi*e_w*AR_w);
+K_t     = 1/(pi*e_t*AR_t);
 
 
 %Eqns 17-24
 CL_w = CL_0_w + CL_alpha_w*alpha;
-CD_w = CD_min_w + K_w*(CL_w^2);     %-what is K_w??
+CD_w = CD_min_w + K_w*(CL_w^2);     
 E = 2*(CL_0_w + CL_alpha_w * (alpha - alpha*(cg_t + cg_w)/V))/(pi*AR_w);
+alpha_t = alpha + i_t + T_e*delta_e + q*cg_t / V - E;
+CL_t = CL_alpha_t*alpha_t;
+CD_t = CD_min_t + K_t*(CL_t^2);
+Cm_f = 
 
-
-
-end
+%end
